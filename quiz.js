4eca7e44721d241247660073105f3312a866d068
hinanshi4 <hinanshis4@gmail.com>
2021-08-15 05:05:37 +0530

    quiz.js

---
+++ quiz.js
@@ -0,0 +1,22 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit-button");
+const outputDiv = document.querySelector("#output");
+
+const correctAnswers = ["90°", "right angled", "32cm", "17.16 meter sq", "12cm"];
+
+function calScore() {
+    let score = 0;
+    let index = 0;
+    const formResult = new FormData(quizForm);
+    for(let value of formResult.values()){
+        // console.log(value);
+        if(value === correctAnswers[index]){
+            score=score+1
+        }
+        index = index + 1;
+    }
+    // console.log(score);
+    outputDiv.innerText = "Your score is " + score;
+}
+
+submitBtn.addEventListener("click", calScore);
