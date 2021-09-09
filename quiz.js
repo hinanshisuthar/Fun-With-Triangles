4619ca1e48ee1fdfd1aff64a891865227b83bdc2
Hinanshi Suthar <88201664+hinanshisuthar@users.noreply.github.com>
2021-09-09 12:02:32 +0530

    added comments

--- quiz.js
+++ quiz.js
@@ -2,14 +2,16 @@ const quizForm = document.querySelector(".quiz-form");
 const submitBtn = document.querySelector("#submit-button");
 const outputDiv = document.querySelector("#output");
 
+//list of correct answers
 const correctAnswers = ["90°", "right angled", "32cm", "17.16 meter sq", "12cm"];
 
 function calScore() {
     let score = 0;
     let index = 0;
     const formResult = new FormData(quizForm);
+    console.log(formResult)
     for(let value of formResult.values()){
-        // console.log(value);
+        console.log(value);
         if(value === correctAnswers[index]){
             score=score+1
         }
