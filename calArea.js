de8331a22e6e96dd9d1f9e04152afd2016c9dc6b
Hinanshi Suthar <88201664+hinanshisuthar@users.noreply.github.com>
2021-09-09 11:05:41 +0530

    added comments

--- calArea.js
+++ calArea.js
@@ -1,16 +1,17 @@
+//referencing html
 const inputBase = document.querySelector("#input-base");
 const inputHeight = document.querySelector("#input-height");
 const checkButton = document.querySelector("#check-area");
 const outputDiv = document.querySelector("#output");
 
 function calArea(base, height){
-    const totalArea = (base*height)*(1/2);
+    const totalArea = (base*height)*(1/2); //formula to cal area
     
     return totalArea;
 }
 
 function submitCheckButton(){
-    var finalAns = calArea(Number(inputBase.value), Number(inputHeight.value));
+    var finalAns = calArea(Number(inputBase.value), Number(inputHeight.value)); //pass two inputs as base and height
     outputDiv.innerText = "the area of the triangle is " + finalAns;
 }
 
