77644fb0ba7dae4f2cdf94be21ba472868c08f3e
hinanshi4 <hinanshis4@gmail.com>
2021-09-16 17:53:34 +0530

    fix: negative input values

--- isTriangle.js
+++ isTriangle.js
@@ -11,6 +11,7 @@ function CalSumOfAngles(angle1, angle2, angle3){
 }
 
 function isTriangle(){
+    if(inputs[0].value > 0 && inputs[1].value && inputs[2].value){ 
     //passing 3 inputs to calculate their sum
     const sumOfAngles = CalSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value)); // 45 45 90
     if(sumOfAngles === 180){
@@ -19,6 +20,10 @@ function isTriangle(){
     else{
         outputDiv.innerText = "Sorry! the angles does not form a triangle";
     }
+}
+else{
+    outputDiv.innerText = "Angles cannot be 0 or -ve";
+}
 }
 
 isTriangleBtn.addEventListener("click", isTriangle);
