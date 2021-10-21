15d5b9722b05eefe10d332c9d4d422a0d020ddf8
Hinanshi Suthar <88201664+hinanshisuthar@users.noreply.github.com>
2021-10-21 17:14:15 +0530

    refactor: added comments and used arrow functions

--- quiz.js
+++ quiz.js
@@ -2,23 +2,19 @@ const quizForm = document.querySelector(".quiz-form");
 const submitBtn = document.querySelector("#submit-button");
 const outputDiv = document.querySelector("#output");
 
-//list of correct answers
-const correctAnswers = ["90°", "right angled", "32cm", "17.16 meter sq", "12cm"];
+const correctAnswers = ["90°", "right angled", "32cm", "17.16 meter sq", "12cm"]; //list of correct answers
 
-function calScore() {
-    let score = 0;
-    let index = 0;
-    const formResult = new FormData(quizForm);
-    console.log(formResult)
-    for(let value of formResult.values()){
+submitBtn.addEventListener("click", () => {
+    let score, index = 0;
+    const formResult = new FormData(quizForm); // a new form data object is created. And it is populated with the current form's key/value pairs
+//     console.log(formResult)
+    for(let value of formResult.values()){ //value=value that is selected the user, fromData.values returns an array of selected answers
         console.log(value);
-        if(value === correctAnswers[index]){
-            score=score+1
+        if(value === correctAnswers[index]){ // if the selected answers match with any of the correctAnswers 
+            score=score+1 // then increment the score
         }
-        index = index + 1;
+        index = index + 1; // check for next index i.e. question
     }
     // console.log(score);
     outputDiv.innerText = "Your score is " + score;
-}
-
-submitBtn.addEventListener("click", calScore);
+});
